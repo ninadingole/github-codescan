39568626895e9bdfa2adaa8d31e8c417648db63c
Ninad <needna78@gmail.com>
2020-07-26 15:04:06 +0530

    Add more sample for scan

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -4,6 +4,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class App {
+    private boolean isAutoSyncScheduled = true;
+
     public static void main(String[] args) throws SQLException {
         SQLInjectionSample c = new SQLInjectionSample();
         List<AccountDTO> accountDTOS = c.unsafeFind("1");
@@ -11,9 +13,24 @@ public class App {
 
 
         ComplexityCode cc = new ComplexityCode();
-        cc.Id();
+        cc.hello();
+
 
         LetsDivideByZero zero = new LetsDivideByZero();
         zero.getAmount();
+
+        App p = new App();
+        p.waitIfAutoSyncScheduled();
     }
+
+    synchronized void waitIfAutoSyncScheduled() {
+        try {
+            while (isAutoSyncScheduled) {
+                this.wait(1000);
+            }
+        } catch (InterruptedException e) {
+            // Expected exception. The file cannot be synchronized yet.
+        }
+    }
+
 }
